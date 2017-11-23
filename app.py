ac9faee2359772e582eaa8c402099bcec7c436c0
Liam Gensel <liamgens@gmail.com>
2017-11-22 23:56:26 -0500

    api now working with semi-working scraper

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from scraper import Scraper
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return "index"
+@app.route('/get-products/<keyword>')
+def index(keyword):
+    scraper = Scraper(keyword)
+    products = scraper.get_products()
+
+    json = {"products": []}
+
+    for product in products:
+        p = Product()
+        p.id = product[0]
+        p.title = product[1]
+        p.image_url = product[2]
+        p.price = product[3]
+        p.review = product[4]
+        json["products"].append(p.serialize())
+
+    return str(json)
+
+
+class Product(db.Model):
+    id = db.Column(db.String(50), primary_key=True)
+    title = db.Column(db.String(1000), nullable=False)
+    image_url = db.Column(db.String(1000))
+    price = db.Column(db.String(25), nullable=False)
+    review = db.Column(db.String(25))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "image_url": self.image_url,
+            "price": self.price,
+            "review": self.review,
+        }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-class Scraper():
+class Scraper:
     def __init__(self, search_term):
         self.search_term = search_term
         self.page = self.fetch_webpage()
+        self.results = self.get_results()
 
     def fetch_webpage(self):
         """Loads results from the search_term.
@@ -12,23 +13,59 @@ class Scraper():
         """
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
         url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=" + self.search_term
+
         try:
             return requests.get(url,headers=headers)
+
         except Exception as e:
             print(e)
+
             return None
 
-    def get_title(self):
-        pass
+    def get_results(self):
+        soup = BeautifulSoup(self.page.content, "html.parser")
+        uls = soup.find_all("ul")
+        results = []
 
-    def get_review(self):
-        pass
+        for ul in uls:
+            lis = ul.find_all('li')
 
-    def get_photo(self):
-        pass
+            for li in lis:
+                if li.get('id'):
+                    results.append(li)
 
-    def get_price(self):
-        pass
+        return results
 
-    def get_product_id(self):
-        pass
+    def get_products(self):
+        products = []
+
+        for i in range(0, len(self.results)):
+            li = self.results[i]
+            product = (self.get_id(li), self.get_title(li), self.get_image(li), self.get_price(li), self.get_review(li))
+            products.append(product)
+
+        return products
+
+    def get_id(self, li):
+        return li['data-asin']
+
+    def get_title(self, li):
+        title = li.find_all('a', {"class": "a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
+        return title[0].get('title') if len(title) > 0 else None
+
+    def get_image(self, li):
+        image = li.find_all('img', {"class": "s-access-image cfMarker"})
+        return image[0].get('src') if len(image) > 0 else None
+
+    def get_price(self, li):
+        price = li.find_all('span', {"class": "sx-price sx-price-large"})
+        if len(price) > 0:
+            whole = price[0].find_all('span')[0].text
+            fraction = price[0].find_all('sup')[1].text
+            price = "$%s.%s" % (whole, fraction)
+            return price
+        return None
+
+    def get_review(self, li):
+        review = li.find_all('span', {"class": "a-icon-alt"})
+        return review[-1].text if len(review) > 0 else None
