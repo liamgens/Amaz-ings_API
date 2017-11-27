74b61e9b64051af3f56fb06f6eb393d06fcb2021
Liam Gensel <liamgens@gmail.com>
2017-11-27 11:46:35 -0500

    added endpoint for fetching larger image url

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from scraper import Scraper
 from models import Product, db
@@ -16,35 +16,60 @@ with app.app_context():
     db.init_app(app)
     db.create_all()
 
-@app.route('/get-products/<keyword>/')
+
+@app.route('/')
+def home():
+    return "home page for testing"
+
+
+@app.route('/get-products/<keyword>')
 def index(keyword):
     scraper = Scraper(keyword)
 
     products = Product.query.filter_by(keyword=keyword).all()
 
-    if not products: # If the products DNE
+    if not products:  # If the products DNE
         products = add_products(scraper)
 
-    elif (datetime.utcnow() - products[0].updated) > timedelta(1): # If the products are over 24 hours old
-        for p in products: # Delete all the exisiting products
+    # If the products are over 24 hours old
+    elif (datetime.utcnow() - products[0].updated) > timedelta(1):
+        for p in products:  # Delete all the exisiting products
             db.session.delete(p)
             db.session.commit()
+
         products = add_products(scraper)
 
     json = {keyword: [p.serialize() for p in products]}
 
-    return str(json)
+    return jsonify(json)
+
+
+@app.route('/get-products')
+def get_products():
+    products = Product.query.all()
+    print(products)
+    json = {"products": [p.serialize() for p in products]}
+
+    return jsonify(json)
+
+
+@app.route('/get-url/<id>')
+def get_url(id):
+    url = Scraper.get_larger_image(id)
+    return jsonify({"url": url})
+
 
 def add_products(scraper):
     products = []
 
-    for i in range(1, 5): # get for the first 4 pages
-        sleep(randint(1,6)) # sleep for random time, between 1-5 secs, to outsmart amazon
+    for i in range(1, 5):  # get for the first 4 pages
+        # sleep for random time, between 1-5 secs, to outsmart amazon
+        sleep(randint(1, 6))
         scraper.fetch_webpage(i)
         scraper.get_results()
         products.extend(scraper.get_products())
 
-    products = list(set(products)) # remove any duplicate products
+    products = list(set(products))  # remove any duplicate products
 
     for p in products:
         db.session.add(p)
--- scraper.py
+++ scraper.py
@@ -12,6 +12,9 @@ class Scraper:
         self.get_results()
 
     def fetch_webpage(self, page=1):
+        """Fetches the raw webpage of product results.
+        Either sets the page field to raw content or None.
+        """
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
         url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&page=" + str(page) + "&field-keywords=" + self.search_term
 
@@ -20,9 +23,11 @@ class Scraper:
 
         except Exception as e:
             print(e)
-            return None
 
     def get_results(self):
+        """Fetch all of the products.
+        Scrapes the webpage for the <li> elems that hold the products.
+        """
         soup = BeautifulSoup(self.page.content, "html.parser")
         uls = soup.find_all("ul")
         results = []
@@ -37,6 +42,11 @@ class Scraper:
         self.results = results
 
     def get_products(self):
+        """Creates a list of products.
+        Goes through every product <li> and fetches it's correct information
+        by calling each individual scraping method.
+        Returns a list of unique products.
+        """
         products = []
         time = datetime.utcnow()
 
@@ -57,17 +67,25 @@ class Scraper:
         return list(set(products)) # remove any duplicate products
 
     def get_id(self, li):
+        """Returns the product's id.
+        """
         return li['data-asin']
 
     def get_title(self, li):
+        """Returns the title of the product.
+        """
         title = li.find_all('a', {"class": "a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal"})
         return title[0].get('title') if len(title) > 0 else None
 
     def get_image(self, li):
+        """Returns the image_url of the product.
+        """
         image = li.find_all('img', {"class": "s-access-image cfMarker"})
         return image[0].get('src') if len(image) > 0 else None
 
     def get_price(self, li):
+        """Returns the price of the product.
+        """
         price = li.find_all('span', {"class": "sx-price sx-price-large"})
 
         if len(price) > 0:
@@ -80,5 +98,30 @@ class Scraper:
         return None
 
     def get_review(self, li):
+        """Returns the review of the product.
+        """
         review = li.find_all('span', {"class": "a-icon-alt"})
         return review[-1].text if len(review) > 0 else None
+
+    def get_larger_image(id):
+        """Returns the larger image url of a product.
+        Takes in an id and scrapes it's webpage for a larger image of the product.
+        Returns the larger url image.
+        """
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+        url = "https://www.amazon.com/dp/" + str(id)
+
+        try:
+            page = requests.get(url,headers=headers)
+            soup = BeautifulSoup(page.content, "html.parser")
+            soup = soup.find_all("img")
+
+            for img in soup:
+                if img.get("data-old-hires"):
+                    return str(img.get("data-old-hires"))
+
+            return None
+
+        except Exception as e:
+            print(e)
+            return None
